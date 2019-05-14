aa9cd3e6d0109c77e86dd5ae4373dc8e8b3d4e6f
Katarinaarntsen <46678311+Katarinaarntsen@users.noreply.github.com>
2019-05-14 15:40:42 +0200

    Animation doors and ship
    made with probably wrong unity version, game_assets_3master, cloned with zip not in desktop (john)

---
+++ Assets/Code/TriggerAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+
+    public bool play = false;
+    public bool playing = false;
+
+    public Animation moveShip;
+    public Animation openDoors;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (play == true && playing == false)
+        {
+            playing = true;
+            play = false;
+            moveShip.Play();
+            openDoors.Play();
+
+        }
+
+    }
+}
